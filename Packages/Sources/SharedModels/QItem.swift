0b608b22ab3922562e2c27f64cff46006a76770d
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-04-02 00:04:44 +0200

    Deleted `DummyModel` and added `QItem` models

---
+++ Packages/Sources/SharedModels/QItem.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+enum QItemType: String, Codable, Sendable {
+    case page = "Page"
+    case section = "Section"
+    case question = "Question"
+}
+
+enum QQuestionType: String, Codable, Sendable {
+    case text
+    case image
+}
+
+struct QItem: Identifiable, Codable, Sendable {
+    let id: String // changed from UUID to match "page1", "q1", etc.
+    let type: QItemType
+    let title: String?
+    let children: [QItem]?
+
+    let questionType: QQuestionType?
+    let content: String?
+    let imageURL: URL?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case type = "itemType"
+        case title
+        case children = "items"
+        case questionType
+        case content = "text"
+        case imageURL = "imageUrl"
+    }
+}
+
+struct QItemList: Codable {
+    let items: [QItem]
+}
+
+
+extension QItem {
+    var displayTitle: String {
+        title ?? content ?? ""
+    }
+
+    var hasVisibleTitle: Bool {
+        !(displayTitle.isEmpty)
+    }
+}
