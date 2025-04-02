a7704eae971e8d5a019658edc098812a16a76781
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-04-02 11:24:15 +0200

    Updated `DataService` to use protocol

--- Packages/Sources/DataFeature/DataService.swift
+++ Packages/Sources/DataFeature/DataService.swift
@@ -1,20 +1,28 @@
 import Foundation
 import SharedModels
 
+public protocol URLSessionProtocol: Sendable {
+    func data(from url: URL) async throws -> (Data, URLResponse)
+}
+
+extension URLSession: URLSessionProtocol {}
+
 public actor DataService {
     private let apiURL: URL = {
         // swiftlint:disable:next force_unwrapping
         return URL(string: "https://run.mocky.io/v3/1800b96f-c579-49e5-b0b8-49856a36ce39")!
     }()
     private let persistenceManager: PersistenceManaging
-
-    public init(persistenceManager: PersistenceManaging) {
+    private nonisolated let urlSession: URLSessionProtocol
+    
+    public init(persistenceManager: PersistenceManaging, urlSession: URLSessionProtocol = URLSession.shared) {
         self.persistenceManager = persistenceManager
+        self.urlSession = urlSession
     }
 
     public func fetchData() async throws -> [QItem] {
         do {
-            let (data, _) = try await URLSession.shared.data(from: apiURL)
+            let (data, _) = try await urlSession.data(from: apiURL)
             let items = try JSONDecoder().decode([QItem].self, from: data)
 
             await persistenceManager.saveItems(items)
